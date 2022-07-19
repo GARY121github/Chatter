f7aec4a66391a4b15a02550a26f0ddc5264ea006
GARY <harshprakash000@gmail.com>
2022-07-20 03:13:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chattingapp/PhoneNumberActivity.java
@@ -0,0 +1,16 @@
+package com.example.chattingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+
+    }
+}
